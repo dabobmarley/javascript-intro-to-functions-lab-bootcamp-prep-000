cf1b302b1b85dc95778693ae89670d7021756a3e
dabobmarley@hotmail.com <dabobmarley@hotmail.com>
2018-05-16 13:47:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,11 @@ function logShout(string) {
 function sayHiToGrandma(string) {
   if (string === lowercase)
     return "I can't hear you!";
-   } else if (string === uppercase)
+   } 
+   else if (string === uppercase)
     return "YES INDEED!";
-    } else if (string === "I love you, Grandma.")
+    } 
+    else if (string === "I love you, Grandma.")
     return "I love you, too."
 }
 
