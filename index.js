b2f89c66bb16b364f90ebca6dfba5c47124daba3
dabobmarley@hotmail.com <dabobmarley@hotmail.com>
2018-05-16 13:48:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ function logShout(string) {
   console.log(string.toLowerCase())}
 function sayHiToGrandma(string) {
   if (string === lowercase)
-    return "I can't hear you!";
-   } 
+    return "I can't hear you!";} 
    else if (string === uppercase){
     return "YES INDEED!";
     } 
