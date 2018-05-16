8faceeec79ba32d4e965055341be9ae2dd53b971
dabobmarley@hotmail.com <dabobmarley@hotmail.com>
2018-05-16 13:30:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ function whisper(string) {
 function sayHiToGrandma(string) {
   if (string) lowercase
     return "I can't hear you!"
-if (string) uppercase
-    return "YES INDEED!"
 
+if else (string) "I love you, Grandma."
+    return " I love you, too.
 }
 function sayHiToGrandma(string) {
-if (string) "I love you, Grandma."
-    return " I love you, too.
+if (string) uppercase
+    return "YES INDEED!"
 }
 
 
